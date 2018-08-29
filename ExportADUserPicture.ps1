b196f9c8d5d93397eedf97efb9c924fc501e1873
Steve Gilissen <steve@gilissen.me>
2018-08-29 11:00:33 +0200

    ExportADUserPicture

---
+++ ExportADUserPicture.ps1
@@ -0,0 +1,15 @@
+﻿function Get-UserPhoto(){
+    param(
+        [string]$Username,
+        [string]$Path = "C:\Temp\AD_Photos"
+    )
+    
+    Import-Module ActiveDirectory
+    $user = Get-ADUser $Username -properties thumbnailPhoto
+    if(!(test-path $Path)){mkdir $Path}
+    $user.thumbnailPhoto | Set-Content ("$Path\$($User.SamAccountName).jpg") -Encoding byte
+}
+$UserInput = Read-Host 'Username?'
+
+$user = Get-UserPhoto $UserInput
+
